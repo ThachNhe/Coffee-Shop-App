4848ca38b4bc1d1de007bff4e17902944b919020
duyhehehe <duy20036athcsns@gmail.com>
2024-04-17 21:58:02 +0700

    create review api

---
+++ server/routes/review_route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const reviewController = require("../controllers/ReviewController");
+const router = express.Router();
+
+router.post("/reviews/:productId/create", reviewController.createReview);
+router.get("/reviews/product/:productId",reviewController.getReviewsOfProduct);
+router.get("/reviews/user/:userId",reviewController.getReviewsOfUser);
+
+module.exports = router;
