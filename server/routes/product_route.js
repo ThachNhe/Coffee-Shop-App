10e6440d5048c0c3d109e518eedb2562742b02b6
duyhehehe <duy20036athcsns@gmail.com>
2024-04-28 01:57:31 +0700

    add api get coffee, bean

--- server/routes/product_route.js
+++ server/routes/product_route.js
@@ -3,7 +3,9 @@ const productController = require("../controllers/ProductController");
 const router = express.Router();
 
 router.post("/products/createProduct", productController.createNewProduct);
-router.get("/products/:productId", productController.getProductById)
+router.get("/products/bean", productController.getAllBeans);
+router.get("/products/coffee", productController.getAllCoffees);
+router.get("/products/:productId", productController.getProductById);
 router.get("/products", productController.getAllProducts);
 
 module.exports = router;
