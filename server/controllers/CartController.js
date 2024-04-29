678a121cd1591a518aa7ef07ff8cb3c9e4ecb62d
duyhehehe <duy20036athcsns@gmail.com>
2024-04-30 00:45:26 +0700

    Add to cart, get my cart api

---
+++ server/controllers/CartController.js
@@ -0,0 +1,126 @@
+const Cart = require("../models/cart");
+const Product = require("../models/product");
+const User = require("../models/user");
+const {ObjectId} = require("mongodb");
+
+class CartController {
+
+    //POST /cart/addToCart
+    async addToCart(req, res) {
+        try {
+            const userId = req.session.User;
+            if (!userId) {
+                return res.status(401).json({
+                    msg: "Login first",
+                });
+            }
+            const productId = req.body.productId;
+            const size = req.body.size;
+            const quantity = req.body.quantity;
+            const product = await Product.findOne({_id: productId});
+            if (!product) {
+                return res.status(404).json({
+                    msg: "Product not found",
+                });
+            }
+            const existingCart = await Cart.findOne({user_id: userId});
+            if (existingCart) {
+                await Cart.updateOne(
+                    {user_id: userId},
+                    {
+                        $addToSet: {
+                            products: {
+                                product_id: productId,
+                                quantity: quantity,
+                                size: size,
+                            }
+                        },
+                    },
+                );
+                return res.status(200).json({
+                    msg: "Product added to the cart",
+                });
+            } else {
+                await Cart.create({
+                    user_id: userId,
+                    products: [{
+                        product_id: productId,
+                        quantity: quantity,
+                        size: size,
+                    }],
+                });
+                return res.status(200).json({
+                    msg: "New cart created and product added",
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    //GET /cart/myCart
+    async getMyCart(req, res) {
+        return res.json(await getCart(req.session.User));
+    }
+}
+
+async function getCart(userId) {
+    try {
+        return await Cart.aggregate([{
+                $match:
+                    {
+                        user_id: new ObjectId(userId),
+                    },
+            }, {
+                $unwind: "$products",
+            }, {
+                $lookup: {
+                    from: "products",
+                    localField: "products.product_id",
+                    foreignField: "_id",
+                    as: "product_info",
+                },
+            }, {
+                $unwind: "$product_info",
+            }, {
+                $project: {
+                    _id: 1,
+                    user_id: 1,
+                    product: {
+                        product_id: "$products.product_id",
+                        quantity: "$products.quantity",
+                        size: {
+                            $filter: {
+                                input: "$product_info.prices",
+                                as: "price",
+                                cond: {
+                                    $eq: ["$$price.size", "$products.size"],
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+                {
+                    $unwind: "$product.size",
+                },
+                {
+                    $addFields: {
+                        "product.cost": {$multiply: ["$product.quantity", "$product.size.price"]}
+                    }
+                }, {
+                    $group: {
+                        _id: "$_id",
+                        user_id: {$first: "$user_id"},
+                        products: {$push: "$product"},
+                        cost: {$sum: "$product.cost"},
+                    }
+                }
+            ]
+        );
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+module.exports = new CartController();
