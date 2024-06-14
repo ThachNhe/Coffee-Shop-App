c9105d9895e9dde671dd583ac2d2a483308f4e3c
duyhehehe <duy20036athcsns@gmail.com>
2024-06-14 22:04:07 +0700

    update api reviews, payment

--- server/routes/index_route.js
+++ server/routes/index_route.js
@@ -3,7 +3,6 @@ const userRoute = require("./user_route");
 const reviewRoute = require("./review_route");
 const cartRoute = require("./cart_route");
 const paymentRoute = require("./payment_route");
-const orderRoute = require("./order_route");
 
 function routesInit(app) {
     app.use("/", productRoute);
@@ -11,7 +10,6 @@ function routesInit(app) {
     app.use("/", reviewRoute);
     app.use("/", cartRoute);
     app.use("/", paymentRoute);
-    app.use("/", orderRoute);
     app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
